19729b4cd604cf9ab45fb38ec91bdf11a654cd2f
Admin <ryan1351995@gmail.com>
2023-12-18 00:49:22 +0700

    Authentication

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
 
@@ -36,17 +38,44 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
         DrawerLayout drawer = binding.drawerLayout;
         NavigationView navigationView = binding.navView;
+
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
                 .setOpenableLayout(drawer)
                 .build();
+
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
+
+        // Bổ sung phần xử lý khi chọn mục menu "Log Out"
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                int id = item.getItemId();
+
+                if (id == R.id.nav_logout) {
+                    // Xử lý đăng xuất ở đây
+                    // Ví dụ: Intent đến màn hình đăng nhập
+                    Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                    finish(); // Đóng màn hình hiện tại
+                }
+
+                // Đặt trạng thái chọn cho NavigationView
+                item.setChecked(true);
+
+                // Đóng DrawerLayout
+                drawer.closeDrawers();
+
+                return true;
+            }
+        });
     }
 
     @Override
---
+++ app/src/main/java/com/example/myapplication/RegisterActivity.java
@@ -0,0 +1,118 @@
+package com.example.myapplication;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class RegisterActivity extends AppCompatActivity {
+    public static final String TAG ="TAG";
+    EditText mFullName, mEmail, mPassword, mConfirmPass;
+    Button mRegisterBtn;
+    TextView mLoginBtn;
+    FirebaseAuth fAuth;
+    ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        mFullName = findViewById(R.id.fullName);
+        mEmail = findViewById(R.id.Email);
+        mPassword = findViewById(R.id.password);
+        mConfirmPass = findViewById(R.id.confirmPass);
+        mRegisterBtn = findViewById(R.id.registerBtn);
+        mLoginBtn = findViewById(R.id.createText);
+        progressBar = findViewById(R.id.progressBar);
+
+        fAuth = FirebaseAuth.getInstance();
+        mRegisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = mEmail.getText().toString();
+                String password = mPassword.getText().toString();
+                String fullName = mFullName.getText().toString();
+                String confirmPass = mConfirmPass.getText().toString();
+
+                if (TextUtils.isEmpty(fullName)){
+                    mFullName.setError("Full name is required");
+                    return;
+                }
+                if (TextUtils.isEmpty(email)){
+                    mEmail.setError("Email name is required");
+                    return;
+                }
+                if (TextUtils.isEmpty(password)){
+                    mPassword.setError("Password is required");
+                    return;
+                }
+                if (password.length() <6){
+                    mPassword.setError("Password must be >=6 characters");
+                    return;
+                }
+                if (confirmPass.isEmpty() || !password.equals(confirmPass)){
+                    mConfirmPass.setError("Invalid Password");
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+                fAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            Toast.makeText(RegisterActivity.this, "User Create", Toast.LENGTH_SHORT).show();
+                            startActivitysecond();
+
+                            FirebaseUser fuser = fAuth.getCurrentUser();
+                            fuser.sendEmailVerification().addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void unused) {
+                                    Toast.makeText(RegisterActivity.this, "Verification Email has been Sent", Toast.LENGTH_SHORT).show();
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.d(TAG,"onFailure: Email not sent" + e.getMessage());
+                                }
+                            });
+
+                        }
+                        else {
+                            Toast.makeText(RegisterActivity.this, "Error !"+ task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+                });
+            }
+        });
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(), LoginActivity.class));
+            }
+        });
+    }
+    private void startActivitysecond(){
+        Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK|Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+    }
+}
